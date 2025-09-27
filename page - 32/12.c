f07d77871f0e051170c614de07d11bda203975d5
ishan gauri <ishangauri506@gmail.com>
2025-09-27 18:27:13 +0530

    Add files via upload

---
+++ page - 32/12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    char ch;
+
+    printf("Enter a character : ");
+    scanf("%c",&ch);
+
+    if (ch >= 'A' && ch <= 'Z') 
+    {
+        printf("%c is a capital latter. \n", ch);
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("%c is a small latter. \n", ch);
+    }
+    else if (ch >= '1' && ch <= '9')
+    {
+        printf("%c is a digit. \n", ch);
+    }
+    else{
+        printf("%c is a special symbol. \n", ch);
+    }
+    
+    return 0;
+
+}
