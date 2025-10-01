e1c99225fdcdcfdc9505917e58455eaa636b4a07
ishan gauri <ishangauri506@gmail.com>
2025-10-01 18:15:02 +0530

    Add files via upload

---
+++ page - 40/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+
+    printf("enter a number for n term \n");
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d", i);
+    }
+    
+    return 0;
+
+}
