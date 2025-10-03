e2ff08ab92acd85b058c8f803387d3d958f390ad
ishan gauri <ishangauri506@gmail.com>
2025-10-03 18:37:45 +0530

    Add files via upload

---
+++ page - 40/7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter number = ");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+        if (i%2!=0)
+        {
+            printf("%d ",1);
+        }
+        else{
+            printf("%d ",-1);
+        }
+    }
+    return 0;
+}
